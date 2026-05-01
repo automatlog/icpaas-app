5d56f171f550e10e6d1e0d0b2177abb3d25aa026
amit kushwaha <107460209+amitkushwaha156@users.noreply.github.com>
2026-05-01 14:33:07 +0530

    ,MFDS

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import 'abort-controller/polyfill';
+import { TextEncoder, TextDecoder } from 'text-encoding';
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder;
+}
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder;
+}
+
 import { registerRootComponent } from 'expo';
 
 import App from './App';
